bccdc32e8d1928eb299eb87ffef587847b057dac
Mani Mozaffar <mani.mozaffar@gmail.com>
2024-10-10 23:45:05 +0200

    Update faulty script for sqlalchemy

---
+++ problems_in_sqla/complicated_update.py
@@ -0,0 +1,116 @@
+# it seems SQLAlchemy tries to figure out what changes happened to database, and try to apply it to the tracked objects from Sessoin
+# if update query is too complicated, it just fail
+# then worst thing happens, it shadow a completely different object as result of database
+# but if you run the query with connection(CORE), it works and return corrected result.
+
+
+from contextlib import asynccontextmanager
+from datetime import UTC, datetime, timedelta
+from uuid import uuid4
+
+import sqlalchemy as sa
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase, Mapped, MappedAsDataclass, mapped_column
+
+USE_CORE = False
+
+
+class BaseSQL(MappedAsDataclass, DeclarativeBase): ...
+
+
+class Event(BaseSQL):
+    __tablename__ = "Events"
+    event: Mapped[str] = mapped_column(sa.String())
+    created_at: Mapped[datetime] = mapped_column(sa.DateTime(timezone=True))
+    id: Mapped[str] = mapped_column(
+        primary_key=True, default_factory=lambda: str(uuid4())
+    )
+    author_id: Mapped[str] = mapped_column(
+        primary_key=True, default_factory=lambda: str(uuid4())
+    )
+
+    __table_args__ = (
+        sa.Index(
+            "unique_active_status_new",
+            author_id,  # per patient
+            created_at,  # per time
+            unique=True,  # only one
+        ),
+    )
+
+
+@asynccontextmanager
+async def init_db():
+    engine = create_async_engine("sqlite+aiosqlite:///:memory:", echo=False)
+    db = async_sessionmaker(engine, expire_on_commit=False, autobegin=False)
+
+    async with engine.begin() as conn:
+        await conn.run_sync(BaseSQL.metadata.create_all)
+    yield db
+
+
+async def test():
+    async with init_db() as db:
+        async with db.begin() as session:
+            now = datetime.now(UTC)
+            author_id = str(uuid4())
+            objs = [
+                Event(
+                    author_id=author_id, event="1", created_at=now - timedelta(days=4)
+                ),
+                Event(
+                    author_id=author_id, event="2", created_at=now - timedelta(days=3)
+                ),
+                Event(
+                    author_id=author_id, event="3", created_at=now - timedelta(days=2)
+                ),
+                Event(
+                    author_id=author_id, event="4", created_at=now - timedelta(days=1)
+                ),
+            ]
+
+            session.add_all(objs)
+            await session.flush()
+
+            # updating the event that comes before the event with the value '2' to value '2'
+            event_lead = sa.select(
+                Event.id,
+                sa.func.lead(Event.event)
+                .over(
+                    partition_by=Event.author_id,
+                    order_by=Event.created_at,
+                )
+                .label("next_event"),
+            ).subquery()
+            event_query = (
+                sa.select(event_lead.c.id)
+                .select_from(event_lead)
+                .where(event_lead.c.next_event == "2")
+            )
+            query = (
+                sa.update(Event)
+                .where(Event.id.in_(event_query))
+                .values(event="2")
+                .returning(Event)
+            )
+
+            if USE_CORE:
+                # updated object is correctly returned (has event='2')
+                conn = await session.connection()
+                result = (await conn.execute(query)).mappings().all()
+                print(result)
+                assert result[0]["event"] == "2"  # NICE!
+
+            else:
+                # updated object is not returned (has event='1') !!!!!!
+                result = (await session.execute(query)).scalars().all()
+                print(result)  # updated object
+                print(objs)  # previous added objects are out of bound!
+
+                assert result[0].event == "2"  # WTF?
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(test())
---
+++ problems_in_sqla/pk_failed_in_view.py
@@ -0,0 +1,228 @@
+# SQLAlchemy ORM is always expecting you to define a primary key
+# but sometimes you don't have one, like in this case, where we are querying a view
+# so what happens? SQLAlchemy tries to figure out what primary key exists in the view.
+# And somehow it picks a wrong primary key (in case we are querying a database that have multiple primary keys)
+# This primary key then is used to identify the object and keep only one instance of it in the session
+# and then, doesn't matter how many rows you query, it will always return the same object
+
+# but if you run the query with connection(CORE), it works and return corrected result.
+
+import typing
+from contextlib import asynccontextmanager
+from datetime import UTC, datetime, timedelta
+from typing import Any, Self
+from uuid import uuid4
+
+import sqlalchemy as sa
+from rich import print
+from sqlalchemy.exc import NoInspectionAvailable
+from sqlalchemy.ext import compiler
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase, Mapped, MappedAsDataclass, mapped_column
+from sqlalchemy.schema import DDLElement
+
+USE_CORE = False
+
+
+class BaseSQL(MappedAsDataclass, DeclarativeBase):
+    def __init_subclass__(cls, *args, **kw) -> None:
+        if hasattr(cls, "__table__"):
+            table_clause = cls.__table__  # type: ignore
+            if isinstance(table_clause, View):
+                for k in cls.__annotations__:
+                    annotation = cls.__annotations__[k]
+                    annotation = typing.get_args(annotation)[0]  # remove Mapped
+                    cls.__annotations__[k] = annotation
+
+        super().__init_subclass__(*args, **kw)
+
+
+class CreateView(DDLElement):
+    def __init__(self, name: str, selectable: sa.Select):
+        self.name = name
+        self.selectable = selectable
+
+
+class DropView(DDLElement):
+    def __init__(self, name, cascade=False, if_exists=False):
+        self.name = name
+        self.cascade = cascade
+        self.if_exists = if_exists
+
+
+@compiler.compiles(CreateView)
+def _create_view(element: CreateView, compiler, **kw):
+    return 'CREATE VIEW "%s" AS %s' % (
+        element.name,
+        compiler.sql_compiler.process(element.selectable, literal_binds=True),
+    )
+
+
+@compiler.compiles(DropView)
+def _drop_view(element: DropView, compiler, **kw):
+    text = "DROP VIEW "
+    if element.if_exists:
+        text += "IF EXISTS "
+    text += f'"{element.name}"'
+    if element.cascade:
+        text += " CASCADE"
+    return text
+
+
+def view_exists(ddl, target, connection, **kw):
+    try:
+        return ddl.name in sa.inspect(connection).get_view_names()
+    except NoInspectionAvailable:
+        return False
+
+
+def view_doesnt_exist(ddl, target, connection, **kw):
+    return not view_exists(ddl, target, connection, **kw)
+
+
+class View(sa.TableClause):
+    exclude_in_sqlite: bool = False
+    inherit_cache: bool = False  # type: ignore
+
+    @classmethod
+    def from_name(cls, name: str, *columns: sa.ColumnClause[Any], **kw: Any) -> Self:
+        return cls(name, *columns, **kw)
+
+
+def view(name, metadata, selectable, *, cascade=True):
+    t = View.from_name(name)
+    t._columns._populate_separate_keys(
+        col._make_proxy(t) for col in selectable.selected_columns
+    )
+    sa.event.listen(
+        metadata,
+        "after_create",
+        CreateView(name, selectable).execute_if(callable_=view_doesnt_exist),  # type: ignore
+    )
+    sa.event.listen(
+        metadata,
+        "before_drop",
+        DropView(name, cascade=cascade).execute_if(callable_=view_exists),  # type: ignore
+    )
+    return t
+
+
+class Event(BaseSQL):
+    __tablename__ = "Events"
+    event: Mapped[str] = mapped_column(sa.String())
+    created_at: Mapped[datetime] = mapped_column(sa.DateTime(timezone=True))
+    id: Mapped[str] = mapped_column(
+        primary_key=True, default_factory=lambda: str(uuid4())
+    )
+    author_id: Mapped[str] = mapped_column(
+        primary_key=True, default_factory=lambda: str(uuid4())
+    )
+
+    __table_args__ = (
+        sa.Index(
+            "unique_active_status_new",
+            author_id,  # per patient
+            created_at,  # per time
+            unique=True,  # only one
+        ),
+    )
+
+
+def event_period_view_query():
+    event_transitions = sa.select(
+        Event.author_id,
+        Event.event,
+        Event.created_at,
+        sa.func.lead(Event.event)
+        .over(
+            partition_by=Event.author_id,
+            order_by=Event.created_at,
+        )
+        .label("next_event"),
+        sa.func.lead(Event.created_at)
+        .over(
+            partition_by=Event.author_id,
+            order_by=Event.created_at,
+        )
+        .label("next_created_at"),
+    ).subquery()
+
+    event_periods = sa.select(
+        event_transitions.c.author_id,
+        event_transitions.c.event,
+        event_transitions.c.created_at.label("start_datetime"),
+        sa.func.coalesce(event_transitions.c.next_created_at, sa.func.now()).label(
+            "end_datetime"
+        ),
+    ).select_from(event_transitions)
+    return event_periods
+
+
+class EventPeriod(BaseSQL):
+    __tablename__ = "EventPeriods"
+    __table__ = view(__tablename__, BaseSQL.metadata, event_period_view_query())
+
+    # id: Mapped[str]
+    author_id: Mapped[str]
+    event: Mapped[str]
+    start_datetime: Mapped[datetime]
+    end_datetime: Mapped[datetime]
+
+
+@asynccontextmanager
+async def init_db():
+    engine = create_async_engine("sqlite+aiosqlite:///:memory:")
+    db = async_sessionmaker(engine, expire_on_commit=False, autobegin=False)
+
+    async with engine.begin() as conn:
+        await conn.run_sync(BaseSQL.metadata.create_all)
+    yield db
+
+
+async def test():
+    async with init_db() as db:
+        async with db.begin() as session:
+            now = datetime.now(UTC)
+            author_id = str(uuid4())
+            session.add_all(
+                [
+                    Event(
+                        author_id=author_id,
+                        event="1",
+                        created_at=now - timedelta(days=3),
+                    ),
+                    Event(
+                        author_id=author_id,
+                        event="2",
+                        created_at=now - timedelta(days=2),
+                    ),
+                    Event(
+                        author_id=author_id,
+                        event="3",
+                        created_at=now - timedelta(days=1),
+                    ),
+                ]
+            )
+            await session.flush()
+            query = sa.select(EventPeriod).order_by(EventPeriod.event)
+
+            if USE_CORE:
+                conn = await session.connection()
+                result = (await conn.execute(query)).mappings().all()
+                print(result)
+                assert result[0]["event"] == "1"
+                assert result[1]["event"] == "2"
+                assert result[2]["event"] == "3"
+
+            else:
+                result = (await session.scalars(query)).all()
+                print(result)
+                assert result[0].event == "1"
+                assert result[1].event == "1"  # WTF?
+                assert result[2].event == "1"  # WTF?!
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(test())
